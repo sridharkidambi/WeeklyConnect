611ccd0ecbca81e7e88547e0861a0740232e0840
sridharkidambi <sridharhere@gmail.com>
2016-08-15 19:50:31 -0500

    Login page sample.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
-
+import {LoginComponent} from './login';
 @Component({
   moduleId: module.id,
   selector: 'app-root',
   templateUrl: 'app.component.html',
-  styleUrls: ['app.component.css']
+  styleUrls: ['app.component.css'],
+  directives: [ LoginComponent ]
 })
 export class AppComponent {
   title = 'Weekly-Connect Web Automation';
